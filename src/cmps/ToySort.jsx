94c0800b97901560b930885e5ee7d3ced35b9235
Moshik Alper <moshikalper@gmail.com>
2024-12-03 18:23:18 +0200

    Add sorting

--- src/cmps/ToySort.jsx
+++ src/cmps/ToySort.jsx
@@ -1,24 +1,16 @@
-import { useEffect, useState } from 'react'
-
-export function ToySort({ filterBy, onSetFilter }) {
+export function ToySort({ sortBy, onSetSort }) {
 
     function handleChange({ target }) {
         const field = target.name
         const value = target.type === 'number' ? +target.value : target.value
-        
-        onSetFilter(prevFilter => ({
-            ...prevFilter,
-            sortBy: {
-                ...prevFilter.sortBy,
-                [field]: field === 'desc' ? -prevFilter.sortBy.desc : value,
-            },
-        }))
+
+        const newSortBy = { ...sortBy, [field]: field === 'desc' ? -sortBy.desc : value }
+        onSetSort(newSortBy)
     }
-    const sortBy = filterBy.sortBy
-    // console.log('sortBy:', sortBy)
+
     return (
         <div className="sort-container">
-            <select name="type" value={sortBy.type} onChange={handleChange}>
+            <select name="type" value={sortBy.type || ''} onChange={handleChange}>
                 <option value="">Sort by</option>
                 <option value="txt">Name</option>
                 <option value="price">Price</option>
--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -6,16 +6,15 @@ import { ToyList } from '../cmps/ToyList.jsx'
 import { LoaderWrapper } from '../cmps/LoaderWrapper.jsx'
 import { toyService } from '../services/toy.service.js'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
-import { loadToys, removeToy, removeToyOptimistic, saveToy, setFilter} from '../store/actions/toy.actions.js'
-import { SET_FILTER_BY } from '../store/reducers/toy.reducer.js'
+import { loadToys, removeToy, removeToyOptimistic, saveToy, setFilter } from '../store/actions/toy.actions.js'
 import { ToySort } from '../cmps/ToySort.jsx'
 import { PaginationButtons } from '../cmps/PaginationButtons.jsx'
 
 export function ToyIndex() {
     const toys = useSelector(storeState => storeState.toyModule.toys)
-    const isLoading = useSelector(storeState => storeState.toyModule.isLoading)
     const filterBy = useSelector(storeState => storeState.toyModule.filterBy)
-    const dispatch = useDispatch()
+    const isLoading = useSelector(storeState => storeState.toyModule.isLoading)
+    // const dispatch = useDispatch()
     // console.log('ToyIndex rendered', toys)
 
     useEffect(() => {
@@ -27,7 +26,11 @@ export function ToyIndex() {
     }, [filterBy])
 
     function onSetFilter(filterBy) {
-        dispatch({ type: SET_FILTER_BY, filterBy })
+        setFilter({ ...filterBy, pageIdx: 0 })
+    }
+
+    function onSetSort(sortBy) {
+        setFilter({ ...filterBy,sortBy: {...sortBy} })
     }
 
     function setPageIdx(pageIdx) {
@@ -68,14 +71,14 @@ export function ToyIndex() {
                 showErrorMsg('Cannot update toy')
             })
     }
-
+    
     return (
         <div>
             <h3>Toys App</h3>
             <main>
                 <button onClick={onAddToy}>Add Random toy ⛐</button>
                 <ToyFilter filterBy={filterBy} onSetFilter={onSetFilter} />
-                <ToySort filterBy={filterBy} onSetFilter={onSetFilter} />
+                <ToySort sortBy={filterBy.sortBy} onSetSort={onSetSort} />
                 <LoaderWrapper isLoading={isLoading}>
                     <ToyList
                         toys={toys}
@@ -83,12 +86,11 @@ export function ToyIndex() {
                         onEditToy={onEditToy}
                     />
                 </LoaderWrapper>
-
-            <PaginationButtons
-                pageIdx={filterBy.pageIdx}
-                setPageIdx={setPageIdx}
-                toysLength={toys.length}
-            />
+                <PaginationButtons
+                    pageIdx={filterBy.pageIdx}
+                    setPageIdx={setPageIdx}
+                    toysLength={toys.length}
+                />
             </main>
         </div>
     )
